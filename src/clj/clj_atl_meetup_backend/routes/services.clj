23c57ccb32903e735f570f9d3aa0371c9628ae95
Jonathon McKitrick <jmckitrick@gmail.com>
2018-10-13 13:42:04 -0400

    Remove obsolote routes

--- src/clj/clj_atl_meetup_backend/routes/services.clj
+++ src/clj/clj_atl_meetup_backend/routes/services.clj
@@ -9,9 +9,9 @@
 
 (def my-distance
   {:destination_addresses
-   ["101 W Chapel Hill St #300, Durham, NC 27701, USA"],
+   ["101 W Chapel Hill St #300, Durham, NC 27701"],
    :origin_addresses
-   ["17 Executive Park Dr NE, Atlanta, GA 30329, USA"],
+   ["17 Executive Park Dr NE, Atlanta, GA 30329"],
    :rows
    [{:elements
      [{:distance {:text "600 km", :value 600437},
@@ -110,38 +110,6 @@ See: https://developers.google.com/maps/documentation/distance-matrix/intro
 
 
       ;;(get-distance-response)
-      ))
-
-  (comment
-    (context "/api" []
-      :tags ["thingie"]
-
-      (GET "/plus" []
-        :return       Long
-        :query-params [x :- Long, {y :- Long 1}]
-        :summary      "x+y with query-parameters. y defaults to 1."
-        (ok (+ x y)))
-
-      (POST "/minus" []
-        :return      Long
-        :body-params [x :- Long, y :- Long]
-        :summary     "x-y with body-parameters."
-        (ok (- x y)))
-
-      (GET "/times/:x/:y" []
-        :return      Long
-        :path-params [x :- Long, y :- Long]
-        :summary     "x*y with path-parameters"
-        (ok (* x y)))
-
-      (POST "/divide" []
-        :return      Double
-        :form-params [x :- Long, y :- Long]
-        :summary     "x/y with form-parameters"
-        (ok (/ x y)))
-
-      (GET "/power" []
-        :return      Long
-        :header-params [x :- Long, y :- Long]
-        :summary     "x^y with header-parameters"
-        (ok (long (Math/pow x y)))))))
+      )
+    )
+  )
